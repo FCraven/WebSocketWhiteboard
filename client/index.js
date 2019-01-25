f266ebb65b7037948109daf6fee27be0e95db5bb
FCraven <craven.francis@gmail.com>
2019-01-24 19:33:06 -0500

    changed npm start script to include webpack -w

--- client/index.js
+++ client/index.js
@@ -3,9 +3,16 @@
 //   An export named `events`, calling it `whiteboard`.
 import whiteboard, {draw} from './whiteboard'
 
+
 // Example: Draw a single stroke.
 draw([0, 0], [250, 250], 'red', true)
 
 whiteboard.on('draw',(start,end,strokeColor)=> {
   console.log(start, end, strokeColor)
+})
+
+const socket = io(window.location.origin)
+
+socket.on('connect', function(){
+  console.log('I have made a persistent two-way connection to the server!')
 })

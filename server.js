261793587db7c8840a6851a0c15dd4d53490f9e2
FCraven <craven.francis@gmail.com>
2019-01-24 19:19:10 -0500

    installed socketIO package created variable io as socketio(server) and turned on event listener for connection event

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 const path = require('path')
 const express = require('express')
 const app = express()
+const socketio = require('socket.io')
 
 // app.listen() returns an http.Server object
 // http://expressjs.com/en/4x/api.html#app.listen
 const server = app.listen(1337, () => {
   console.log(`Listening on http://localhost:${server.address().port}`)
 })
+
+const io =  socketio(server)
 
 app.use(express.static(path.join(__dirname, 'public')))
